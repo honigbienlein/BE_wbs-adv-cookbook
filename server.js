5347ec2ffd37e3f4f0a4d329a20ecb802810cfa2
Melis Augsburger <melis.augsburger@outlook.com>
2022-07-27 18:41:51 +0200

    img of recipes fetched

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ import cors from "cors";
 
 const app = express();
 const port = 8000;
+//const path = require('path');
 
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.use(cors());
 
 app.get('/', (req, res) => {
